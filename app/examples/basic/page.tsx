473816426f55816520f0dbb611439370e9114b0c
Shane Liebenthal <shaneliebenthal@gmail.com>
2023-11-01 17:04:23 -0400

    Remove explicit type from basic example as defaultValues helps to infer type for useAutoSave

--- app/examples/basic/page.tsx
+++ app/examples/basic/page.tsx
@@ -4,7 +4,7 @@ import { useAutoSave } from "@/hooks/useAutoSave";
 import { useForm } from "react-hook-form";
 
 const Page = () => {
-  const form = useForm<{ name: string }>({
+  const form = useForm({
     defaultValues: {
       name: "",
     },
@@ -17,21 +17,15 @@ const Page = () => {
   });
 
   return (
-    <div className="min-h-[70vh] container mx-auto px-6">
-      <h1 className="text-2xl font-semibold my-8">Basic Example</h1>
+    <div>
+      <h1>Basic Example</h1>
       <p>
         <strong>Debounced Value:</strong> {JSON.stringify(debouncedValue.name)}
       </p>
       <form>
-        <div className="flex flex-col gap-2 mt-4">
-          <label className="text-white font-semibold" htmlFor="name">
-            Name
-          </label>
-          <input
-            className="w-full rounded bg-slate-800 px-2 py-1 text-white"
-            type="text"
-            {...register("name")}
-          />
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" {...register("name")} />
         </div>
       </form>
     </div>
